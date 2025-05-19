e52aa992ed952b6416e02728e1c831da87e850ce
Xuan Bao <work.xuanbao@gmail.com>
2025-05-19 16:57:16 +0700

    init

---
+++ src/test/java/vn/warehouse/WarehousewebApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
